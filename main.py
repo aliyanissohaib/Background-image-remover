d5b7fe05399d4d63322a593f07684f3019eb88e5
Muhammad Aliyan Sohaib <108171361+aliyanissohaib@users.noreply.github.com>
2024-03-25 23:14:05 +0500

    output added

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# import easygui
+ import easygui
 # from rembg import remove
 # from PIL import Image
 #
-# input_path = easygui.fileopenbox(title='Select Image File')
-# output_path = easygui.filesavebox(title='Save file to..')
+ input_path = easygui.fileopenbox(title='Select Image File')
+ output_path = easygui.filesavebox(title='Save file to..')
 # image_input= Image.open(input_path)
 # output = remove(input_path)
 # output.save(output_path)
-from rembg import remove
 
-
-from PIL import Image
 
 img = Image.open("D:\\python\\bgremover\\input\\a.jpg")
 
