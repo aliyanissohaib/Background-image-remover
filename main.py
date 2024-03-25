4f2b5560238328e8d55962c0fb1c47586f0cc599
Muhammad Aliyan Sohaib <108171361+aliyanissohaib@users.noreply.github.com>
2024-03-25 23:17:06 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ from PIL import Image
 
 input_path = easygui.fileopenbox(title='Select Image File')
 output_path = easygui.filesavebox(title='Save file to..')
-# image_input= Image.open(input_path)
-# output = remove(input_path)
-# output.save(output_path)
+
 
 
 img = Image.open(input_path)
